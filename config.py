4fe924f31cf3f59b63767cb34e0131d2e23569f8
i.nechakhin <i.nechakhin@g.nsu.ru>
2023-09-28 10:34:06 +0000

    Create folder config for rule.json and fuzz.json(-result working pre_fuzz.py) => Change config.py

--- config.py
+++ config.py
@@ -2,7 +2,8 @@ import os
 import json
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-RULE_PATH = BASE_DIR + '/rule.json'
+RULE_PATH = BASE_DIR + '/config/rule.json'
+FUZZ_PATH = BASE_DIR + '/config/fuzz.json'
 
 with open(RULE_PATH, 'r') as f:
     CONFIG_RULES = json.load(f)
