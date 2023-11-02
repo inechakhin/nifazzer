d09325552fee97cc3257e3c788edb3ea936ddc1a
i.nechakhin <i.nechakhin@g.nsu.ru>
2023-11-02 15:29:00 +0000

    Add new def: reformat_rule. Add this method in def download_rfc. Def expand_rule_list change link on another RFC for generator.

--- abnf/abnf_parser.py
+++ abnf/abnf_parser.py
@@ -1,7 +1,7 @@
 import os
 import re
 import requests
-from config import BASE_DIR
+from config import RFC_DIR
 from abnf.util import find_index_of_char
 
 
@@ -12,12 +12,19 @@ class Abnf_Parser:
     def get_rule_list(self) -> dict:
         return self.rule_list
 
+    def __reformat_rule(self, rule: str) -> str:
+        if "see [RFC" in rule:
+            return rule.replace("&lt;", "<").replace("&gt;", ">")
+        else:
+            return rule
+
     # download and save the abnf list from a given rfc-link
     def __download_rfc(self, rfc_number: str) -> None:
-        html_content = requests.get(
-            "https://www.rfc-editor.org/rfc/rfc" + rfc_number
-        ).content
+        start_url = "https://www.rfc-editor.org/rfc/rfc"
+        html_content = requests.get(start_url + rfc_number).content
         real_html = html_content.decode("utf8")
+        pattern = re.compile(r"<.+?>", re.S)
+        real_html = pattern.sub("", real_html)
 
         regex_start_abnf = re.compile("[ ]*[a-zA-Z0-9-]*[ ]* = .*")
         regex_end_abnf = re.compile("")
@@ -28,21 +35,21 @@ class Abnf_Parser:
         i = 0
         while i < len_content_list:
             if regex_start_abnf.match(content_list[i]) != None:
-                abnf_list.append(content_list[i])
+                abnf_list.append(self.__reformat_rule(content_list[i]))
                 if i < len_content_list:
                     while regex_end_abnf.fullmatch(content_list[i + 1]) == None:
-                        abnf_list.append(content_list[i + 1])
+                        abnf_list.append(self.__reformat_rule(content_list[i + 1]))
                         i += 1
             i += 1
         abnf = "\n".join(abnf_list)
-        file_path = BASE_DIR + "/abnf/rfc/rfc" + rfc_number + ".txt"
+        file_path = RFC_DIR + "rfc" + rfc_number + ".txt"
         with open(file_path, "w") as f:
             f.write(abnf)
         return
 
     # read the abnf list from a given file
     def parse_rule_list(self, rfc_number: str) -> None:
-        file_path = BASE_DIR + "/abnf/rfc/rfc" + rfc_number + ".txt"
+        file_path = RFC_DIR + "rfc" + rfc_number + ".txt"
         if not os.path.exists(file_path):
             self.__download_rfc(rfc_number)
 
@@ -76,12 +83,17 @@ class Abnf_Parser:
         rfc_list = []
         for key in self.rule_list[rfc_number]:
             rule = self.rule_list[rfc_number][key]
-            if "see [RFC" not in rule:
-                continue
             reg = re.compile(".*<(?P<name>[^,]*), .*RFC(?P<rfc_num>[0-9]*).*")
-            res = reg.match(rule).groupdict()
-            if res["rfc_num"] not in rfc_list:
-                rfc_list.append(res["rfc_num"])
+            match = reg.match(rule)
+            if match == None:
+                continue
+            else:
+                res = match.groupdict()
+                rfc_name = res["name"]
+                rfc_num = res["rfc_num"]
+                if rfc_num not in rfc_list:
+                    rfc_list.append(rfc_num)
+                self.rule_list[rfc_number][key] = "<" + rfc_name + "RFC" + rfc_num + ">"
         for rfc_number in rfc_list:
-            self.parse_rule_list(self, rfc_number)
+            self.parse_rule_list(rfc_number)
         return
